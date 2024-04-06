bc4e014efaff45c26a8e26f0ac36c36deeb5b639
Bon <Blackball007b@gmail.com>
2024-04-06 14:17:15 +0700

    Added Ruby and Raw Ruby

--- src/main/java/net/revalorise/honeybar/Honeybar.java
+++ src/main/java/net/revalorise/honeybar/Honeybar.java
@@ -2,6 +2,7 @@ package net.revalorise.honeybar;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.revalorise.honeybar.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,8 @@ public class Honeybar implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+
+		ModItems.registerModItems();
+
 	}
 }
